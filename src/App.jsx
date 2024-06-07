c4a53496e802cabcc8048b04568a727c717bd4fc
Kamla Mistry <kamla@mistry-family.co.uk>
2024-06-07 10:23:23 +0100

    use toastify to add out of money message and take zombieFighters out of state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,80 @@
 import './App.css'
 import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const zombieFighters = [
+  {
+    name: 'Survivor',
+    price: 12,
+    strength: 6,
+    agility: 4,
+    img: 'https://via.placeholder.com/150/92c952',
+  },
+  {
+    name: 'Scavenger',
+    price: 10,
+    strength: 5,
+    agility: 5,
+    img: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    name: 'Shadow',
+    price: 18,
+    strength: 7,
+    agility: 8,
+    img: 'https://via.placeholder.com/150/24f355',
+  },
+  {
+    name: 'Tracker',
+    price: 14,
+    strength: 7,
+    agility: 6,
+    img: 'https://via.placeholder.com/150/d32776',
+  },
+  {
+    name: 'Sharpshooter',
+    price: 20,
+    strength: 6,
+    agility: 8,
+    img: 'https://via.placeholder.com/150/1ee8a4',
+  },
+  {
+    name: 'Medic',
+    price: 15,
+    strength: 5,
+    agility: 7,
+    img: 'https://via.placeholder.com/150/66b7d2',
+  },
+  {
+    name: 'Engineer',
+    price: 16,
+    strength: 6,
+    agility: 5,
+    img: 'https://via.placeholder.com/150/56acb2',
+  },
+  {
+    name: 'Brawler',
+    price: 11,
+    strength: 8,
+    agility: 3,
+    img: 'https://via.placeholder.com/150/8985dc',
+  },
+  {
+    name: 'Infiltrator',
+    price: 17,
+    strength: 5,
+    agility: 9,
+    img: 'https://via.placeholder.com/150/392537',
+  },
+  {
+    name: 'Leader',
+    price: 22,
+    strength: 7,
+    agility: 6,
+    img: 'https://via.placeholder.com/150/602b9e',
+  },
+]
 
 const App = () => {
 
@@ -7,85 +82,11 @@ const App = () => {
   const [money, setMoney] = useState(100)
   const [strength, setStrength] = useState(0)
   const [agility, setAgility] = useState(0)
-
-  const [zombieFighters, setZombieFighters] = useState([
-    {
-      name: 'Survivor',
-      price: 12,
-      strength: 6,
-      agility: 4,
-      img: 'https://via.placeholder.com/150/92c952',
-    },
-    {
-      name: 'Scavenger',
-      price: 10,
-      strength: 5,
-      agility: 5,
-      img: 'https://via.placeholder.com/150/771796',
-    },
-    {
-      name: 'Shadow',
-      price: 18,
-      strength: 7,
-      agility: 8,
-      img: 'https://via.placeholder.com/150/24f355',
-    },
-    {
-      name: 'Tracker',
-      price: 14,
-      strength: 7,
-      agility: 6,
-      img: 'https://via.placeholder.com/150/d32776',
-    },
-    {
-      name: 'Sharpshooter',
-      price: 20,
-      strength: 6,
-      agility: 8,
-      img: 'https://via.placeholder.com/150/1ee8a4',
-    },
-    {
-      name: 'Medic',
-      price: 15,
-      strength: 5,
-      agility: 7,
-      img: 'https://via.placeholder.com/150/66b7d2',
-    },
-    {
-      name: 'Engineer',
-      price: 16,
-      strength: 6,
-      agility: 5,
-      img: 'https://via.placeholder.com/150/56acb2',
-    },
-    {
-      name: 'Brawler',
-      price: 11,
-      strength: 8,
-      agility: 3,
-      img: 'https://via.placeholder.com/150/8985dc',
-    },
-    {
-      name: 'Infiltrator',
-      price: 17,
-      strength: 5,
-      agility: 9,
-      img: 'https://via.placeholder.com/150/392537',
-    },
-    {
-      name: 'Leader',
-      price: 22,
-      strength: 7,
-      agility: 6,
-      img: 'https://via.placeholder.com/150/602b9e',
-    },
-  ])
-
   const [searchValue, setSearchValue] = useState(null)
 
   function handleAddFighter(fighter) {
     if (fighter.price > money) {
-      console.log('Not enough money!')
+      toast("Not enough money!")
     } else {
       const newTeam = structuredClone(team)
       newTeam.push(fighter)
@@ -102,7 +103,7 @@ const App = () => {
     setTeam(newTeam)
     setMoney(money + fighter.price)
     setStrength(strength - fighter.strength)
-    setAgility(agility - fighter.agility)    
+    setAgility(agility - fighter.agility) 
   }
 
   function logInput(e) {
@@ -116,12 +117,15 @@ const App = () => {
     } else {
         const regex = new RegExp(`${searchValue}`, 'i')
         return regex.test(fighter.name)
+        //  or can write as:
+        // fighter.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
     }
-
 }) 
 
   return (<>
     <h1>Zombie Fighters</h1>
+
+    < ToastContainer />
 
     <h3>Money: {money}</h3>
     <h3>Team Strength: {strength}</h3>
@@ -147,7 +151,7 @@ const App = () => {
     <div className='fightersDiv'>
       {filteredFighters.map((fighter, index) => {
         return <ul key={index}>
-          <img src={fighter.img}/>
+          <img src={fighter.img} alt={fighter.name}/>
           <li>{fighter.name}</li>
           <li>Price: {fighter.price}</li>
           <li>Strength: {fighter.strength}</li>
